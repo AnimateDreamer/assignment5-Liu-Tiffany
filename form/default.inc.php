82867fa52397da87415c927864620946c7269f0a
Tiffany Liu <tiffany.liu@ufl.edu>
2020-12-03 16:07:05 -0800

    Contact form is styled

---
+++ form/default.inc.php
@@ -0,0 +1,18 @@
+
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="chocolate.html">Chocolates</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+
+        </ul>  
+    </nav>      
+</header>
+
+<div class="container">
+    <h1>Hello Sunshine! The world isn't ready to say hello yet</h1>
+    <p>Please try again later and we'll be ready</p>
+</div>
